c6c33bd3b1dbf07eed480dd28e7c2529f4ba916a
shahinsharbaf <88193227+shahinsharbaf@users.noreply.github.com>
2023-05-19 09:37:14 +0330

    fina

--- app/page.js
+++ app/page.js
@@ -1,6 +1,9 @@
 import Image from "next/image";
+import { db } from "@vercel/postgres";
 
 export default async function Home() {
+  const client = await db.connect();
+  const pets = await client.sql`SELECT * FROM Pets;`;
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
@@ -8,6 +11,7 @@ export default async function Home() {
           Get started by editing&nbsp;
           <code className="font-mono font-bold">app/page.js</code>
         </p>
+        <h1>hello {pets.rows[0].name}</h1>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
